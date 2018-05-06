c65bf89f1b859115f9d9a053b1578100128e9430
Grzegorz Szermiński <szerman@poczta.fm>
2018-05-06 20:43:10 +0200

    Exercise

---
+++ Public/scripts/app.js
@@ -0,0 +1,47 @@
+console.log('app.js is running');
+
+//var template = <div>Czt to widać</div>;
+//var template = React.createElement ('div', {}, 'Grześ');
+
+/*var usersList = 'Dupa Jasiu';	
+var templateTwo = React.createElement ('div', {},
+	React.createElement ('h1', {}, usersList.toUpperCase()),
+	React.createElement ('ol', {},
+		React.createElement ('li', {}, 'Jestem Szermindre'),
+		React.createElement ('li', {}, 'Super gość') 
+	),
+);
+
+var titleList = {
+	title: 'Star Wars',
+	subtitle: 'odcinek 2'
+};
+var templateThree = React.createElement ('div', {},
+	React.createElement ('h2', {}, titleList.title),
+	React.createElement ('p', {}, titleList.subtitle)
+); 
+*/
+
+var user = {
+	name: 'Grześ',
+	age: '37',
+	location: 'Wrocław'
+};
+
+function getLocation (location) {
+	if (location) {
+		return location;
+	} else {
+		return 'Unknown'
+	}
+};
+
+var templateTwo = React.createElement ('div', {},
+	React.createElement ('h1', {}, user.name.toUpperCase()),
+	React.createElement ('p', {}, 'age: ', user.age),
+	React.createElement ('p', {}, 'location: ', getLocation(user.location)),
+);
+
+var appRoot = document.getElementById('app');
+ReactDOM.render(templateTwo, appRoot);
+//ReactDOM.render(template, appRoot);
