75867bb087ba78bd419b0cff64f94404535138d9
Grzegorz Szermiński <szerman@poczta.fm>
2018-06-30 08:54:13 +0200

    JS-exercise - Change styles

--- Public/scripts/app.js
+++ Public/scripts/app.js
@@ -23,9 +23,11 @@ var templateThree = React.createElement ('div', {},
 */
 
 var user = {
+	title: 'Indension App',
 	name: 'Grześ',
 	age: '37',
-	location: 'Wrocław'
+	location: 'Wrocław',
+	options: ['one', 'two']
 };
 
 function getLocation (location) {
@@ -34,12 +36,24 @@ function getLocation (location) {
 	} 
 };
 
-var templateTwo = React.createElement ('div', {},
-	React.createElement ('h1', {}, user.name ? user.name : 'Anonymus'),
-	React.createElement ('p', {}, 'age: ', user.age),
+function getAge (age) {
+	if (age >= 18) {
+		return 'age: ' + age;
+	} else {
+		return 'You are under 18';
+	} 
+};
+
+
+const templateTwo = React.createElement ('div', {},
+	React.createElement ('h1', {}, user.title),
+	React.createElement ('h3', {}, user.name ? user.name.toUpperCase() : 'Anonymus'),
+	React.createElement ('p', {}, user.age ? getAge(user.age) : 'please enter your age'),
 	React.createElement ('p', {}, getLocation(user.location)),
+	React.createElement ('p', {}, user.options.length > 0 ? 'Here are some options' : 'No options'),
+	123
 );
 
-var appRoot = document.getElementById('app');
+const appRoot = document.getElementById('app');
 ReactDOM.render(templateTwo, appRoot);
 //ReactDOM.render(template, appRoot);
